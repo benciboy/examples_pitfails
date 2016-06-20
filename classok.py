44973c346693fe732e9a3ec41b2ae423bf874bdd
Benedek Kocsis <benedek.kocsis@ericsson.com>
2016-06-20 12:50:28 +0200

    atnevezes

--- classok.py
+++ classok.py
@@ -6,7 +6,6 @@ class Parent(object):
         self.age = age
 
 if __name__ == '__main__':
-
     A = [Parent('Bela'+str(i), i*i) for i in range(5)]
     for i in A:
         print i.name, i.age
--- generators_lambdas.py
+++ generators_lambdas.py
@@ -1,1 +1,9 @@
-sdfsadfgasdf
+# Build and return a list
+def firstn(n):
+    num, nums = 0, []
+    while num < n:
+        nums.append(num)
+        num += 1
+    return nums
+
+sum_of_first_n = sum(firstn(1000000))
