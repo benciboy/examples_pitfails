63d69dcdb3240df0fa173edaa9a96253ac9d18e4
Benedek Kocsis <benedek.kocsis@ericsson.com>
2016-09-12 18:11:13 +0200

    Dice read added

--- lottos.py
+++ lottos.py
@@ -1,5 +1,6 @@
-import scipy
 from scipy.special import comb
+import csv
+
 def combtoindex(arg):
     ''' az argumentum lista kombinaciohoz tarozo index kiszamitasa  '''
     arg.reverse()
@@ -13,6 +14,49 @@ def combtoindex(arg):
     return int(result)
 
 
+def indextocomb( ind, k=6, n=45):
+    subs = k
+    var = 0
+    komb = []
+    for var in range(k):
+
+        while ( n>=subs ) and ( ind < comb(n,subs) ):
+                n-=1
+
+        if (n>=subs):
+                komb.append(n+1)
+                ind = ind - comb(n,subs)
+        else:
+                komb.append(subs)
+
+        subs-=1
+
+    komb.reverse()
+    return komb
+
+
+with open('/home/ebenkoc/Downloads/hatos.csv', 'rb') as f:
+    reader = csv.reader(f, delimiter=';', quoting=csv.QUOTE_NONE)
+    szamok=[]
+    for row in reader:
+        szamok.append(row[-7:-1])
+
+
+def getmaxgap(items):
+    ''' return from a sorted number list the max gap and the interval  '''
+    maxgap = 0
+    for i in range(1, len(items)):
+        localmax = max(maxgap, items[i] - items[i - 1])
+        if (localmax > maxgap):
+            maxgap = localmax
+            maxgapitem = i
+
+    return maxgap, items[maxgapitem - 1], items[maxgapitem]
+
+
+# a = [10, 20, 24, 26, 27, 300, 2900]
+# print getmaxgap(a)
+# getmaxgap.__doc__
 
 a=[6, 18, 23, 34, 37, 44]
 a=[1,2,3,4,5,6]
@@ -23,3 +67,5 @@ b=[1,2,3,4,89]
 c = combtoindex(indextocomb(485654)), indextocomb(6554565)
 print c
 #print comb(amax[0],len(amax)), scipy.special.binom(amax[0],len(amax))
+
+print len(szamok), szamok[0]
